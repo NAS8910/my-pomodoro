6a52c71b3a6327cdd17464a0a6d3a7056d2671aa
Om Gaikwad <62072438+omgaikwad@users.noreply.github.com>
2022-05-16 11:18:43 +0530

    fix: started signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-// import LandingPage from "./pages/LandingPage/LandingPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
 import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
     <div className="App bg-serene-white ">
-      <Signup />
+      <LandingPage />
     </div>
   );
 }

f3e6c15a1f7b391ea199ce5920d1d90e6075e6df
Om Gaikwad <62072438+omgaikwad@users.noreply.github.com>
2022-05-27 13:35:58 +0530

    update: guest user credentials

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -8,10 +8,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshBalika123",
+    firstName: "Om",
+    lastName: "Gaikwad",
+    email: "omgaikwad228@gmail.com",
+    password: "omgaikwad123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
